2a96576d229148295699b45efaab188cd40b52ef
Greg <aquamirror@gmail.com>
2021-10-21 09:27:22 -0700

    broken down my project

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Work Day Scheduler Starter Code
+
+GitHub hosted page: https://phonecologne.github.io/Work-Day-Scheduler/
---
+++ assets/js/javascript.js
@@ -0,0 +1,10 @@
+// document.createElement  hourly time blocks
+// saving to local storage
+var timeelement = $("div")
+var container = $(".container")
+container.append(timeelement)
+
+// 4 elements in each container: time color, save button, text input and current time of time
+
+// breaking up each day into 9am to 5pm
+// current time is a different color
