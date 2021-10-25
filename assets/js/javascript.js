81a52085e69e12b4802d606420fe272eb45c7115
Greg <aquamirror@gmail.com>
2021-10-24 19:27:31 -0700

    just gotta start from square 1

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,10 +1,14 @@
-// document.createElement  hourly time blocks
-// saving to local storage
-var timeelement = $("div")
-var container = $(".container")
-container.append(timeelement)
+var currentDay = moment().format("dddd, MMM Do");
 
-// 4 elements in each container: time color, save button, text input and current time of time
+var hour9 = moment().hour(9);
+var hour10 = moment().hour(10);
+var hour11 = moment(). hour(11);
+var hour12 = moment().hour(12);
+var hour13 = moment(). hour(13);
+var hour14 = moment(). hour(14);
+var hour15 = moment(). hour(15);
+var hour16 = moment(). hour(16);
+var hour17 = moment(). hour(17);
+var hour18 = moment(). hour(18);
 
-// breaking up each day into 9am to 5pm
-// current time is a different color
+var hours = [hour9, hour10, hour11, hour12, hour13]
